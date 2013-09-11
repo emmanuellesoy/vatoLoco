8b8f0332fdc731dc28dde05b0d5d6af4ff6c81a8
Jorge Olvera <jorge@emprend.me>
2013-09-11 14:17:30 -0500

    Maquetado y funcionalidad basica

--- header.php
+++ header.php
@@ -3,6 +3,8 @@
 <head>
 	<title><?php bloginfo('name'); ?></title>
 	<meta charset="utf-8" />
+	<link rel="stylesheet" type="text/css" href="<?php bloginfo('template_directory'); ?>/css/cholos.css">
+	<link rel="stylesheet" type="text/css" href="<?php bloginfo('template_directory'); ?>/css/normalize.css">
 	<script src="//ajax.googleapis.com/ajax/libs/jquery/1.10.2/jquery.min.js"></script>
     <script type="text/javascript" src="<?php bloginfo('template_directory'); ?>/js/prefixfree.min.js"></script>
     <script type="text/javascript" src="<?php bloginfo('template_directory'); ?>/js/modernizr.js"></script>
